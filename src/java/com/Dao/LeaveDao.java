137e426dae6cdd0633d1d2705a3ceb376ee4580f
KheXin <s65519@ocean.umt.edu.my>
2025-04-28 22:17:11 +0800

    Implement the reporting module

--- src/java/com/Dao/LeaveDao.java
+++ src/java/com/Dao/LeaveDao.java
@@ -5,8 +5,10 @@
 package com.Dao;
 
 import com.Model.Leave;
+import com.Model.Report;
 import java.util.*;
 import java.sql.*;
+import java.sql.Date;
 import util.Database;
 
 public class LeaveDao {
@@ -140,7 +142,7 @@ public class LeaveDao {
 
         try {
             Connection con = Database.getConnection();
-            PreparedStatement ps = con.prepareStatement("SELECT * FROM `leave` WHERE leaveStatus NOT IN ('Pending')");
+            PreparedStatement ps = con.prepareStatement("SELECT * FROM `leave` WHERE leaveStatus NOT IN ('Pending') ORDER BY leaveStartDate DESC");
             ResultSet rs = ps.executeQuery();
             while (rs.next()) {
                 Leave leave = new Leave();
@@ -160,12 +162,12 @@ public class LeaveDao {
         return list;
     }
 
-    public static List<Leave> getAllTodayLeave() {
+    public static List<Leave> getAllTodayUnprocessedLeave() {
         List<Leave> list = new ArrayList<Leave>();
 
         try {
             Connection con = Database.getConnection();
-            PreparedStatement ps = con.prepareStatement("SELECT * FROM `leave` WHERE CURDATE() BETWEEN leaveStartDate AND leaveEndDate");
+            PreparedStatement ps = con.prepareStatement("SELECT * FROM `leave` WHERE CURDATE() BETWEEN leaveStartDate AND leaveEndDate AND leaveStatus='PENDING'");
 
             ResultSet rs = ps.executeQuery();
             while (rs.next()) {
@@ -215,6 +217,154 @@ public class LeaveDao {
         }
 
         return leave;
+    }
+
+    public static List<Report> getLeaveReport(Date startDate, Date endDate) {
+        List<Report> list = new ArrayList<Report>();
+
+        try {
+            Connection con = Database.getConnection();
+            String sql = "SELECT t.teacherName, "
+                    + "COUNT(l.leaveId) AS totalLeaves, "
+                    + " COALESCE(SUM(CASE WHEN l.leaveStatus = 'Approved' THEN DATEDIFF(l.leaveEndDate, l.leaveStartDate) + 1 ELSE 0 END) * 100.0 \n"
+                    + "             / NULLIF(SUM(DATEDIFF(l.leaveEndDate, l.leaveStartDate) + 1), 0), 0) AS approvedPercentage,\n"
+                    + "    COALESCE(SUM(CASE WHEN l.leaveStatus = 'Rejected' THEN DATEDIFF(l.leaveEndDate, l.leaveStartDate) + 1 ELSE 0 END) * 100.0 \n"
+                    + "             / NULLIF(SUM(DATEDIFF(l.leaveEndDate, l.leaveStartDate) + 1), 0), 0) AS rejectedPercentage,\n"
+                    + "    COALESCE(SUM(CASE WHEN l.leaveStatus = 'Pending' THEN DATEDIFF(l.leaveEndDate, l.leaveStartDate) + 1 ELSE 0 END) * 100.0 \n"
+                    + "             / NULLIF(SUM(DATEDIFF(l.leaveEndDate, l.leaveStartDate) + 1), 0), 0) AS pendingPercentage,"
+                    + "COALESCE(SUM(DATEDIFF(l.leaveEndDate, l.leaveStartDate) + 1), 0) AS totalLeaveDays, "
+                    + "COALESCE(SUM(CASE WHEN l.leaveStatus = 'Approved' THEN DATEDIFF(l.leaveEndDate, l.leaveStartDate) + 1 ELSE 0 END), 0) AS totalApprovedLeaveDays "
+                    + "FROM teacher t "
+                    + "LEFT JOIN `leave` l ON t.teacherId = l.absentTeacherId ";
+            if (startDate != null && endDate != null) {
+                sql += "AND (l.leaveStartDate BETWEEN ? AND ? OR l.leaveEndDate BETWEEN ? AND ?) ";
+            }
+            sql += "GROUP BY t.teacherId, t.teacherName ORDER BY t.teacherName ASC";
+
+            PreparedStatement ps = con.prepareStatement(sql);
+            if (startDate != null && endDate != null) {
+                ps.setDate(1, startDate);
+                ps.setDate(2, endDate);
+                ps.setDate(3, startDate);
+                ps.setDate(4, endDate);
+            }
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                Report report = new Report();
+                report.setTeacherName(rs.getString("teacherName"));
+                report.setTotalLeaves(rs.getInt("totalLeaves"));
+                report.setApprovedPercentage(rs.getDouble("approvedPercentage"));
+                report.setRejectedPercentage(rs.getDouble("rejectedPercentage"));
+                report.setPendingPercentage(rs.getDouble("pendingPercentage"));
+                report.setTotalLeaveDays(rs.getInt("totalLeaveDays"));
+                report.setTotalApprovedLeaveDays(rs.getInt("totalApprovedLeaveDays"));
+                list.add(report);
+            }
+            con.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return list;
+    }
+
+    public static List<Report> getLeaveRanking(Date startDate, Date endDate) {
+        List<Report> list = new ArrayList<>();
+        try {
+            Connection con = Database.getConnection();
+            String sql = "SELECT t.teacherId, \n"
+                    + "  COALESCE(SUM(DATEDIFF(l.leaveEndDate, l.leaveStartDate) + 1), 0) AS totalLeaveDays,\n"
+                    + "  (SELECT COALESCE(SUM(DATEDIFF(l2.leaveEndDate, l2.leaveStartDate) + 1), 0) \n"
+                    + "   FROM `leave` l2 \n"
+                    + "   WHERE 1=1";
+
+            if (startDate != null && endDate != null) {
+                sql += " AND (l2.leaveStartDate BETWEEN ? AND ? OR l2.leaveEndDate BETWEEN ? AND ?)";
+            }
+
+            sql += ") AS totalRecords \n"
+                    + "FROM teacher t \n"
+                    + "LEFT JOIN `leave` l ON t.teacherId = l.absentTeacherId AND l.leaveStatus ='Approved'";
+
+            if (startDate != null && endDate != null) {
+                sql += " AND (l.leaveStartDate BETWEEN ? AND ? OR l.leaveEndDate BETWEEN ? AND ?) ";
+            }
+
+            sql += "GROUP BY t.teacherId\n"
+                    + "ORDER BY totalLeaveDays DESC\n"
+                    + "LIMIT 3";
+            PreparedStatement ps = con.prepareStatement(sql);
+
+            if (startDate != null && endDate != null) {
+                ps.setDate(1, startDate);
+                ps.setDate(2, endDate);
+                ps.setDate(3, startDate);
+                ps.setDate(4, endDate);
+                ps.setDate(5, startDate);
+                ps.setDate(6, endDate);
+                ps.setDate(7, startDate);
+                ps.setDate(8, endDate);
+            }
+
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                Report sa = new Report();
+                sa.setTeacherId(rs.getInt("teacherId"));
+                sa.setTotalLeaveDays(rs.getInt("totalLeaveDays"));
+                sa.setTotalLeavesByAllTeachers(rs.getInt("totalRecords"));
+                list.add(sa);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return list;
+    }
+
+    public static List<Report> getLeaveTypePercentage(Date startDate, Date endDate) {
+        List<Report> list = new ArrayList<>();
+        try {
+            Connection con = Database.getConnection();
+            String sql = "SELECT l.leaveReason, \n"
+                    + "COALESCE(COUNT(*) * 100.0 / NULLIF((SELECT COUNT(*) from `leave` WHERE 1=1\n";
+            if (startDate != null && endDate != null) {
+                sql += " AND (leaveStartDate BETWEEN ? AND ? OR leaveEndDate BETWEEN ? AND ?)";
+            }
+
+            sql += "), 0), 0) AS percentage\n"
+                    + "FROM `leave` l\n"
+                    + "WHERE 1=1\n";
+
+            if (startDate != null && endDate != null) {
+                sql += " AND (l.leaveStartDate BETWEEN ? AND ? OR l.leaveEndDate BETWEEN ? AND ?)\n";
+            }
+
+            sql += "GROUP BY l.leaveReason";
+
+            PreparedStatement ps = con.prepareStatement(sql);
+
+            if (startDate != null && endDate != null) {
+                ps.setDate(1, startDate);
+                ps.setDate(2, endDate);
+                ps.setDate(3, startDate);
+                ps.setDate(4, endDate);
+                ps.setDate(5, startDate);
+                ps.setDate(6, endDate);
+                ps.setDate(7, startDate);
+                ps.setDate(8, endDate);
+            }
+
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                Report sa = new Report();
+                sa.setLeaveTypes(rs.getString("leaveReason"));
+                sa.setLeaveTypePercentage(rs.getDouble("percentage"));
+                list.add(sa);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return list;
     }
 
 }
