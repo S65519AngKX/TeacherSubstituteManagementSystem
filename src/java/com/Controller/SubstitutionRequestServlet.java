0fd9d86dacc07fc9fffb58677759dc39d7ccd067
KheXin <s65519@ocean.umt.edu.my>
2025-06-17 16:35:52 +0800

    Fix redirect issue in SubstitutionRequestServlet

--- src/java/com/Controller/SubstitutionRequestServlet.java
+++ src/java/com/Controller/SubstitutionRequestServlet.java
@@ -27,7 +27,7 @@ import java.sql.Connection;
 import java.sql.Date;
 import util.Database;
 
-@WebServlet("/")
+@WebServlet(name = "SubstitutionRequestServlet", urlPatterns = {"/SubstitutionRequestServlet"})
 public class SubstitutionRequestServlet extends HttpServlet {
 
     private SubstitutionRequestDao SubstitutionRequestDao;
